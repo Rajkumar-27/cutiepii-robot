748b2d3f8fab4156962846d4c537847f65d5e28a
NeuroticCoders <87204519+NeuroticCoders@users.noreply.github.com>
2021-08-03 19:34:51 +0530

    `lyrics`

---
+++ Cutiepii_Robot/modules/lyric.py
@@ -0,0 +1,58 @@
+from asyncio import get_running_loop
+from functools import partial
+from pyrogram.types import (InlineQueryResultArticle, InputTextMessageContent)
+from pyrogram import filters
+
+from telegram import Bot, Update, Message, Chat
+from telegram.ext import CallbackContext, run_async
+
+from Cutiepii_Robot.modules.helper_funcs.alternate import typing_action
+from Cutiepii_Robot import arq, dispatcher, pgram as cutiepii
+from Cutiepii_Robot.modules.disable import DisableAbleCommandHandler
+
+@cutiepii.on_message(filters.command("lyrics"))
+async def lyrics_func(_, message):
+    if len(message.command) < 2:
+        return await message.reply_text("**Usage:**\n/lyrics [QUERY]")
+    m = await message.reply_text("**Searching**")
+    query = message.text.strip().split(None, 1)[1]
+    song = await arq.lyrics(query)
+    lyrics = song.result
+    if len(lyrics) < 4095:
+        return await m.edit(f"__{lyrics}__")
+    lyrics = await paste(lyrics)
+    await m.edit(f"**LYRICS_TOO_LONG:** [URL]({lyrics})")
+
+
+async def lyrics_func(answers, text):
+    song = await arq.lyrics(text)
+    if not song.ok:
+        answers.append(
+            InlineQueryResultArticle(
+                title="Error",
+                description=song.result,
+                input_message_content=InputTextMessageContent(
+                    song.result
+                ),
+            )
+        )
+        return answers
+    lyrics = song.result
+    song = lyrics.splitlines()
+    song_name = song[0]
+    artist = song[1]
+    if len(lyrics) > 4095:
+        lyrics = await hastebin(lyrics)
+        lyrics = f"**LYRICS_TOO_LONG:** [URL]({lyrics})"
+
+    msg = f"__{lyrics}__"
+
+    answers.append(
+        InlineQueryResultArticle(
+            title=song_name,
+            description=artist,
+            input_message_content=InputTextMessageContent(msg),
+        )
+    )
+    return answers
+  
